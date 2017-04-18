b24c4e362080fba9599e2ad2d0a58b929e13d9e8
rjthecarguy <rj@rjrobinson.net>
2017-04-17 22:48:27 -0700

    graph color changes

--- src/pages/testosterone-graph/testosterone-graph.ts
+++ src/pages/testosterone-graph/testosterone-graph.ts
@@ -44,24 +44,13 @@ this.barChart = new Chart(this.barCanvas.nativeElement, {
             data: {
                 labels: this.labels,
                 datasets: [{
-                    label: '# of Votes',
+                    label: 'Expressed as pg/ml',
+                    pointRadius: 10,
+                    pointHitRadius: 30,
+                    pointBackgroundColor:'rgba(191, 27, 44, 1)',
                     data: this.data,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
+                    backgroundColor:'rgba(52, 100, 107, 0.5)',
+                    borderColor: 'rgba(52, 100, 107, 1)',
                     borderWidth: 1
                 }]
             },
